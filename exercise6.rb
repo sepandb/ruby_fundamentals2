f2815bc0e021f7e41f466aa915a10247c141a668
Sep Bar <sepand.barkhodaee@gmail.com>
2014-01-15 14:09:27 -0500

    working on exercise 6

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,21 @@
 list = ["carrots", "chips", "apples", "salmon"]
 
-list.each do |x|
-	puts "* #{x}"
+def showlist(array)
+	array.each do |x|
+		puts "* #{x}"
+	end
 end
+
+showlist(list)
+
+def additem(array, item)
+	array.push("#{item}")
+end
+
+puts "What item do you want to add to your list?"
+item = gets.chomp
+additem(list, item)
+
+showlist(list)
+
+puts "There are #{list.length} items on your list!"
