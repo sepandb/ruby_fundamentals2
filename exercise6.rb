9e54b22ffa838919986b4a5ca7f044bb1a5dd620
Sep Bar <sepand.barkhodaee@gmail.com>
2014-01-15 15:21:58 -0500

    Exercise 6 finished

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,7 @@
 list = ["carrots", "chips", "apples", "salmon"]
 
+
+#The method 
 def showlist(array)
 	array.each do |x|
 		puts "* #{x}"
@@ -12,13 +14,13 @@ def additem(array, item)
 	array.push("#{item}")
 end
 
-puts "What item do you want to add to your list?"
+puts "What item do you want to add to the list?"
 item = gets.chomp
 additem(list, item)
 
 showlist(list)
 
-puts "There are #{list.length} items on your list!"
+puts "There are #{list.length} items on the list!"
 
 if list.include?("bananas")
 	puts "Bananas are on the list!"
@@ -26,4 +28,12 @@ else
 	puts "Bananas are NOT on the list!"
 end
 
-puts list[1]
+puts "#{list[1]} is the second item on the list!"
+
+puts "Check out the sorted list below:"
+showlist(list.sort)
+
+puts "Let's remove salmon from the list!"
+list.delete("salmon")
+
+showlist(list.sort)
